6aa603bcb95ad0067c98d420a49a3197d32c2be5
Omochice <mochice.mls.ntl@gmail.com>
2023-12-15 10:38:18 +0900

    chore:

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,22 @@
 import { debug, getInput, info } from "npm:@actions/core@1.10.1";
 import { writeSummary } from "./github.ts";
+import { loadConfig } from "./config.ts";
+import { ok } from "npm:neverthrow@6.1.0"
 
 const main = async () => {
   info("Start main process");
   const message = getInput("config", { required: true });
 
-  try {
-    const json = JSON.parse(Deno.readTextFileSync(message));
-    info(json);
-  } catch (e) {
-    info(e);
-  }
+  loadConfig(message)
+    .andThen((config) => {
+      info(JSON.stringify(config, null, 2));
+      return ok(undefined)
+    })
+    .orElse((err: Error) => {
+      info(err.message);
+      debug(err.stack ?? "");
+      return ok(undefined)
+    });
   await writeSummary("main", "hi?");
 
   info("Complete main process");
