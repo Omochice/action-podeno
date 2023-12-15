b4880da713246308af937ca1cfb3cf7c61365191
Omochice <mochice.mls.ntl@gmail.com>
2023-12-15 13:37:13 +0900

    chore: hmm

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,41 @@
 import { debug, getInput, info } from "npm:@actions/core@1.10.1";
 import { writeSummary } from "./github.ts";
 import { loadConfig } from "./config.ts";
-import { ok } from "npm:neverthrow@6.1.0"
+import { ok, ResultAsync } from "npm:neverthrow@6.1.0";
+import { execPodium } from "https://raw.githubusercontent.com/Omochice/podeno/main/src/lua.ts";
+import { dirname } from "https://deno.land/std@0.209.0/path/posix/dirname.ts";
 
 const main = async () => {
   info("Start main process");
   const message = getInput("config", { required: true });
 
-  loadConfig(message)
-    .andThen((config) => {
-      info(JSON.stringify(config, null, 2));
-      return ok(undefined)
-    })
-    .orElse((err: Error) => {
-      info(err.message);
-      debug(err.stack ?? "");
-      debug(`${err.cause}` ?? "");
-      return ok(undefined)
-    });
+  const configs = loadConfig(message);
+  if (configs.isErr()) {
+    info(configs.error.message);
+    debug(configs.error.stack ?? "");
+    return;
+  }
+
+  for (const c of configs.value) {
+    try {
+      const converted = await execPodium(
+        Deno.readTextFileSync(c.in),
+        c.type,
+      );
+      if (converted.isErr()) {
+        info(converted.error.message)
+      } else {
+        Deno.mkdirSync(dirname(c.out), { recursive: true });
+        Deno.writeTextFileSync(c.out, converted.value);
+      }
+    } catch (e) {
+      if (e instanceof Error) {
+        info(e.message);
+        debug(e.stack ?? "");
+      }
+      debug(`${e}`);
+    }
+  }
   await writeSummary("main", "hi?");
 
   info("Complete main process");
