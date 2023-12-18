9aad570855dc670a7c104242afbb4a1f41bac23b
Omochice <mochice.mls.ntl@gmail.com>
2023-12-18 19:02:32 +0900

    chore: use json5

--- mod.ts
+++ mod.ts
@@ -1,9 +1,9 @@
 // import { summary } from "npm:@actions/core@1.10.1";
-import { parse } from "https://deno.land/std@0.200.0/jsonc/parse.ts"
+import JSON5 from "npm:json5@2.2.3"
 
 console.log("hi this is from docker");
 
-console.log(parse(Deno.args[0]));
+console.log(JSON5.parse(Deno.args[0]));
 
 // summary.addRaw("## header\n\nthis is message.").write();
 
