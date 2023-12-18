3f66ccff5c17815d2053fa5a5ce1acb63da5512f
Omochice <mochice.mls.ntl@gmail.com>
2023-12-18 18:54:19 +0900

    chore: use std module instead

--- mod.ts
+++ mod.ts
@@ -1,17 +1,9 @@
 // import { summary } from "npm:@actions/core@1.10.1";
+import { parse } from "https://deno.land/std@0.200.0/jsonc/parse.ts"
 
 console.log("hi this is from docker");
 
-console.log(Deno.args);
-try {
-  console.log(JSON.parse(Deno.args[0]));
-} catch (e) {
-  console.error(e);
-}
-const j = Deno.args.join(" ");
-console.log(j);
-
-console.log(JSON.parse(j));
+console.log(parse(Deno.args[0]));
 
 // summary.addRaw("## header\n\nthis is message.").write();
 
