c81049946a96bb088972f42b787155e35e6c8411
Omochice <mochice.mls.ntl@gmail.com>
2023-12-18 09:35:51 +0900

    chore: can i save file over jobs?

--- mod.ts
+++ mod.ts
@@ -1,7 +1,9 @@
-import { summary } from "npm:@actions/core@1.10.1";
+// import { summary } from "npm:@actions/core@1.10.1";
 
 console.log("hi this is from docker");
 
 console.log(`[${Deno.args.join(", ")}]`);
 
-summary.addRaw("## header\n\nthis is message.").write();
+// summary.addRaw("## header\n\nthis is message.").write();
+
+Deno.writeTextFileSync("sample.txt", "this is sample.text");
