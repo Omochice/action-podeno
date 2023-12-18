b92f2dab364d166e3fa086c0c7b3d65ab1909f4a
Omochice <mochice.mls.ntl@gmail.com>
2023-12-18 18:40:12 +0900

    chore

--- mod.ts
+++ mod.ts
@@ -2,11 +2,16 @@
 
 console.log("hi this is from docker");
 
-console.log(Deno.args)
-const j = Deno.args.join(" ")
+console.log(Deno.args);
+try {
+  console.log(JSON.parse(Deno.args[0]));
+} catch (e) {
+  console.error(e);
+}
+const j = Deno.args.join(" ");
 console.log(j);
 
-console.log(JSON.parse(j))
+console.log(JSON.parse(j));
 
 // summary.addRaw("## header\n\nthis is message.").write();
 
